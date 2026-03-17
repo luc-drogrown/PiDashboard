0df2804709658adf777359acc46f2561201a2ddf
luc-drogrown <draganlucian18@gmail.com>
2026-03-17 20:52:57 +0200

    Changed file structure

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 from fastapi import FastAPI, Request, Form
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse
+from fastapi.staticFiles import StaticFiles
 from smbus2 import SMBus
 
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
 bus = SMBus(1)
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 @app.get("/", response_class=HTMLResponse)
 async def read_item(request: Request):
